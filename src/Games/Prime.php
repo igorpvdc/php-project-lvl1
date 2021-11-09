bbcdc2af6a565bd2cc2526fd1b5328740b78b909
igor.pvdc <igor.pvdc@gmail.com>
2021-11-09 16:14:25 +0000

    updated composer.json and brain-prime game

--- src/Games/Prime.php
+++ src/Games/Prime.php
@@ -44,6 +44,10 @@ function primeGame($name = '', $array = [])
 
 function isPrime($num)
 {
+    if ($num === 1) {
+        return false;
+    }
+
     for ($i = 2; $i < $num; $i++) {
         if ($num % $i === 0) {
             return false;
